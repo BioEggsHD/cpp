f6f887e5226eb67ab858e77cc9f8327c402ffcb3
BioEggsHD <alisup221@gmail.com>
2024-11-06 16:05:03 +0100

    Create loops.cpp

---
+++ loops.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+    for(int i=1; i<=100;++i){
+        std::cout<<i<<'\n';
+    }
+    int i = 0;
+    while(i<100){
+        ++i;
+        std::cout<<i<<'\n';
+    }
+    return 0;
+}
