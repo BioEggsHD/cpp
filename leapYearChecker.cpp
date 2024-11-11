88f80cee220ed3dc614e51cff403ac6debabaa1a
BioEggsHD <alisup221@gmail.com>
2024-11-11 11:11:17 +0100

    Create leapYearChecker.cpp

---
+++ leapYearChecker.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main(){
+    int yr;
+    std::cin>>yr;
+    if(!(std::cin)){
+        std::cout<<"Invalid input.";
+        return 1;
+    }
+    if(yr%4 == 0){
+        std::cout<<"Leap year!";
+    } else {
+        std::cout << "Not a leap year.";
+    }
+    return 0;
+}
